c3b308b841a74c39a8d97beb08a793dfb451b926
Amy Hogue <hogue.a@gmail.com>
2018-11-29 18:53:50 -0600

    Create SurveyResponse through form

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -3,4 +3,24 @@ class SurveyResponsesController < ApplicationController
     @survey = Survey.find(params[:survey_id])
     @survey_response = @survey.survey_responses.build
   end
+
+  def create
+    @survey = Survey.find(params[:survey_id])
+    @survey_response = SurveyResponse.new(survey_response_params)
+    @survey_response.survey = @survey
+
+    if @survey_response.save
+      head :created
+    else
+      flash.now[:notice] = 'Please enter your email'
+      render :new
+    end
+  end
+
+
+  private
+
+  def survey_response_params
+    params.require(:survey_response).permit(:email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'survey/:survey_id', to: 'survey_responses#new'
 
-  resources :survey_responses, only: :create
+  resources :survey, only: [] do
+    resources :survey_responses, only: :create
+  end
 end
