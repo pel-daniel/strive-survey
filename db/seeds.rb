355573cff5e7fdbc7c1ca193c9e07cf20632de55
Amy Hogue <hogue.a@gmail.com>
2018-11-29 18:01:47 -0600

    Add log messages to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
+puts 'Deleting existing surveys'
+
 Survey.destroy_all
 
+puts 'Creating test survey'
 survey = Survey.create!(name: 'Test Strive Survey')
+
+puts 'Creating 5 questions'
 
 [ 'What\'s your favorite color?',
   'What\'s your pet name?',
@@ -10,3 +15,5 @@ survey = Survey.create!(name: 'Test Strive Survey')
 ].each do |question|
   Question.create!(survey: survey, text: question)
 end
+
+puts 'Done!'
